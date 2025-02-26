934738eebff9e316afb0c232cbe4900cda822c02
Ashutosh Nigam <ashutoshnigam@MacBookPro.router.asus.com>
2025-02-26 14:53:53 +0530

    Updated Hello World:

--- src/hello-world.py
+++ src/hello-world.py
@@ -1,13 +1,19 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 
 st.text("Hello, World!")
+st.text("This is a Streamlit app created by Ashutosh Nigam")
 
-df= pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 24, 3, 27]
+st.link_button("Click here to visit my LinkedIn profile", "https://www.linkedin.com/in/mrashutoshnigam")
+st.balloons()
+st.success("This is a success message")
+# Create a simple dataframe
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
 })
-st.table(df)
-st.write(df)
 
+st.dataframe(df)
+# Create a line chart
 st.line_chart(df)
